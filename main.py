89dcab4050e4151d99fde8578f0b8c56a88c48f0
Oleksandr Shekera <oleksandrshekera@gmail.com>
2023-03-30 10:57:52 +0300

    Created read data function

--- main.py
+++ main.py
@@ -1,4 +1,10 @@
+def read_data(filename):
+    with open(filename, 'r') as f:
+        data = [line.strip(',') for line in f]
+    return data
+
 def main(file_name):
+    data = read_data(file_name)
     return
 if __name__ == '__main':
     main('test.txt')
