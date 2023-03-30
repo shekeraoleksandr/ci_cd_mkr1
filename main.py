ceea6f1f5ec9bc767090ca33135bb1d326dc5e2e
Oleksandr Shekera <oleksandrshekera@gmail.com>
2023-03-30 11:13:31 +0300

    Created sort by area function

--- main.py
+++ main.py
@@ -1,10 +1,19 @@
 def read_data(filename):
     with open(filename, 'r') as f:
-        data = [line.strip(',') for line in f]
+        data = [line.strip().split(',') for line in f]
     return data
+
+
+def sort_by_area(data):
+    sorted_data = sorted(data, key=lambda x: int(x[1]))
+    return sorted_data
 
 def main(file_name):
     data = read_data(file_name)
-    return
-if __name__ == '__main':
+    sorted_by_area = sort_by_area(data)
+    print('Sorted by area: ')
+    print_data(sorted_by_area)
+
+
+if __name__ == '__main__':
     main('test.txt')
