9d0c7399972608d7c4731821bddfafa1235d7ff0
Oleksandr Shekera <oleksandrshekera@gmail.com>
2023-03-30 11:49:30 +0300

    Added tests for sorting functions, created requirments.txt and report.html

--- tests/test_main.py
+++ tests/test_main.py
@@ -1,1 +1,32 @@
 import pytest
+from main import read_data, sort_by_area, sort_by_population
+
+
+@pytest.fixture()
+def sample_data():
+    return [
+        ['USA', '1000', '500'],
+        ['China', '2000', '1000'],
+        ['Russia', '1500', '750'],
+        ['India', '1200', '800']
+    ]
+
+
+def test_sort_by_area(sample_data):
+    expected_sorted_data = [
+        ['USA', '1000', '500'],
+        ['India', '1200', '800'],
+        ['Russia', '1500', '750'],
+        ['China', '2000', '1000']
+    ]
+    assert sort_by_area(sample_data) == expected_sorted_data
+
+
+def test_sort_by_population(sample_data):
+    expected_sorted_data = [
+        ['USA', '1000', '500'],
+        ['Russia', '1500', '750'],
+        ['India', '1200', '800'],
+        ['China', '2000', '1000']
+    ]
+    assert sort_by_population(sample_data) == expected_sorted_data
